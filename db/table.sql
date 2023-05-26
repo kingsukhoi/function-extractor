473901c1c4999037872228560f792bb55edd4e19
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2023-05-26 17:44:43 -0600

    add file table

--- db/table.sql
+++ db/table.sql
@@ -7,3 +7,13 @@ create table functions
     id        uuid default uuid_generate_v4(),
     relpath   text
 );
+
+create table functions
+(
+    abspath   text not null,
+    func_name text not null,
+    body      text not null,
+    type      text,
+    id        uuid default uuid_generate_v4(),
+    relpath   text
+);
